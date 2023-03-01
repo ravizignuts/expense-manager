d1835a5029628831f3ec511ad87c0c1b76525f78
Ravi Thakor <ravit@zignuts.com>
2023-03-01 20:02:42 +0530

    feature : add,edit,list transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\Transaction;
+use Illuminate\Http\Client\ResponseSequence;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -14,9 +16,9 @@ class TransactionController extends Controller
      * @return json Data
      */
     public function add(Request $request){
-        $validator = Validator::make($request,[
+        $validator = Validator::make($request->all(),[
             'account_id'      => 'required|numeric|exists:accounts,id',
-            'account_user_id' => 'required|numeric|exists:account_user_id,id',
+            'account_user_id' => 'required|numeric|exists:account_users,id',
             'type'            => 'required|in:income,expense,transfer',
             'date'            => 'required|date',
             'category'        => 'required|string',
@@ -30,7 +32,7 @@ class TransactionController extends Controller
         }
         $transaction = Transaction::create($request->only('account_id','account_user_id','type','date','category','amount'));
         return response()->json([
-            'success'     => false,
+            'success'     => true,
             'transaction' => $transaction
         ]);
     }
@@ -39,8 +41,27 @@ class TransactionController extends Controller
      * @param Request $request,$id
      * @return json Data
      */
-    public function edit(){
-
+    public function edit(Request $request,$id){
+        $validator = Validator::make($request->all(),[
+            'account_id'      => 'required|numeric|exists:accounts,id',
+            'account_user_id' => 'required|numeric|exists:account_users,id',
+            'type'            => 'required|in:income,expense,transfer',
+            'date'            => 'required|date',
+            'category'        => 'required|string',
+            'amount'          => 'required|numeric'
+        ]);
+        if($validator->fails()){
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors(),
+            ]);
+        }
+        $transaction = Transaction::findOrFail($id);
+        $transaction->update('account_id','account_user_id','type','date','category','amount');
+        return response()->json([
+            'success'     => true,
+            'transaction' => $transaction
+        ]);
     }
     /**
      * API for add Transaction
@@ -63,6 +84,10 @@ class TransactionController extends Controller
      * @return json Data
      */
     public function list(){
-
+        $transaction = Transaction::get();
+        return response()->json([
+            'message'       => 'All Transaction',
+            'Transaction'   => $transaction
+        ]);
     }
 }
